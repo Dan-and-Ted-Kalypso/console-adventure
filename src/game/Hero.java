8577facecdde7eb76bbf41c0bc7d4ac566218d7f
edwardmccormick <ted.mccormick@gmail.com>
2021-01-27 11:29:54 -0600

    Updating hero info

--- src/game/Hero.java
+++ src/game/Hero.java
@@ -1,13 +1,23 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
 
+    public int heroPotion;
     public int healthPoints;
     public String heroName;
+    public List<String> inv = new ArrayList<String>();
+
 
     public Hero(String name) {
 
         this.healthPoints = 50;
         this.heroName = name;
+        this.heroPotion = 2;
+        this.inv.add = "Nothing";
     }
+
+
 }
